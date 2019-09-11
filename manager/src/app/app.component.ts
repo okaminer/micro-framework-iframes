739b301857bb7476292a3cc9d2c7f49c0ffc09d2
Oren Kaminer <oren.kaminer@emc.com>
2019-09-11 15:14:54 +0300

    web ui working asm not working xforward issue

--- manager/src/app/app.component.ts
+++ manager/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {LogsrvService} from './logsrv.service';
-import {generateExpandoInstructionBlock} from '@angular/core/src/render3/instructions';
+import {send} from 'q';
+
 
 @Component({
   selector: 'app-root',
@@ -13,22 +14,54 @@ export class AppComponent implements OnInit {
 
   constructor(private logService: LogsrvService) {
   }
-
+  currentIframe: HTMLIFrameElement;
   ngOnInit() {
     window.addEventListener('message', this.messageHandler.bind(this), { capture: true });
-    document.getElementById('myframe').addEventListener('load', function() {
-      console.log('loaded');
-      document.getElementById('myframe').style.background = "url('')";
-    });
+    this.hideBackgroundOnLoad('exampleFrame');
+    this.hideBackgroundOnLoad('webuiFrame');
+    this.hideBackgroundOnLoad('vxFlexFrame');
+    this.currentIframe = (<HTMLIFrameElement>document.getElementById('exampleFrame'));
   }
 
+  hideBackgroundOnLoad(frameId: string) {
+    document.getElementById(frameId).addEventListener('load', function() {
+      console.log('loaded');
+      document.getElementById(frameId).style.background = "url('')";
+    });
+  }
+  soStuff(sender){
+    switch (sender) {
+      case 'exampleFrame': {
+        this.switchFrames('exampleFrame');
+        break;
+      }
+      case 'webuiFrame': {
+        this.switchFrames('webuiFrame');
+        break;
+      }
+      case 'vxFlexFrame': {
+        this.switchFrames('vxFlexFrame');
+        break;
+      }
+      default:{
+        console.error('Can\'t find frame for ' + sender);
+      }
+
+    }
+  }
+
+  switchFrames(newFrameId: string){
+    this.currentIframe.style.display = 'none';
+    this.currentIframe  = (<HTMLIFrameElement>document.getElementById(newFrameId));
+    this.currentIframe.style.display = 'block';
+  }
   messageHandler(message) {
     this.addLog2('Got message from: ' + message.origin + ' - ' + message.data);
   }
 
 
   onClick() {
-    const iframe = document.getElementById('myframe');
+    const iframe = document.getElementById('webuiFrame');
     (<HTMLIFrameElement>iframe).contentWindow.postMessage('message from parent ', '*');
   }
 
